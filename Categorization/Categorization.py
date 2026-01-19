cc19a7c51697a4d8307af039a037149ac0e587c8
Moriah Goldsmith <moriahtemima@gmail.com>
2026-01-19 13:06:33 +0200

    adding xlsx file and updating func

--- Categorization/Categorization.py
+++ Categorization/Categorization.py
@@ -1,34 +1,19 @@
 import pandas as pd
-from binarization.binarize_category import apply_binary
-def categorization(df,category_dict):
-    new_columns = {}
-    for new_col, old_cols in category_dict.items():
-        #vectored column sum and converting to percentage
-        new_columns[new_col] = ((df[old_cols].sum(axis = 1))/len(old_cols))
-    #casting to df
-
-    return pd.DataFrame(new_columns)
-
-
-data = pd.read_csv("enhanced_anxiety_dataset.csv")
-dicti = {"Lifestyle Factors": ["Physical Activity (hrs/week)", "Alcohol Consumption (drinks/week)","Family History of Anxiety"]}
-config = {
-    "Physical Activity (hrs/week)": {
-        "type": "numeric",
-        "healthy_range": [3,5]
+#from Binarization.binarize_category import apply_binary
+def categorization(binary_df, guideline):
+    categorized_df = {}
+    
+    # שימוש ב-unique כדי לעבור על כל קטגוריה רק פעם אחת
+    unique_categories = guideline["Category"].unique()
+    
+    for cat in unique_categories:
+        # שליחת רשימת כל המשתנים השייכים לקטגוריה הזו
+        variables_in_cat = guideline[guideline["Category"] == cat]["Variable"].tolist()
         
-    },
-    "Alcohol Consumption (drinks/week)": {
-        "type": "numeric",
-        "healthy_range": [5,7]
-       
-    },
-    "Family History of Anxiety": {
-        "type": "binary",
-        "mapping": {"yes": 1, "no": 0}
-    }
-}
+        # סכימת העמודות הרלוונטיות מתוך ה-df הבינארי וחישוב ממוצע (אחוז הצלחה)
+        # axis=1 סוכם שורות, count נותן את מספר המשתנים בקטגוריה
+        categorized_df[cat] = binary_df[variables_in_cat].sum(axis=1) / len(variables_in_cat)
 
-result = apply_binary(data, config)
+    return pd.DataFrame(categorized_df)
 
-#print(f'{categorization(result, dicti).value_counts()}')
+
--- binarization/binarize_category.py
+++ binarization/binarize_category.py
@@ -1,49 +1,36 @@
 import numpy as np
 import pandas as pd
 
-def apply_binary(df, config):
+def apply_binary(df, guidelines):
     df = df.copy()  # לא לשנות את המקור
+    mapping = {"yes": 1, "no": 0}
+    try: 
+        for i, category in guidelines["Variable"].items():
+            try:
+               random = df[category] 
+            except:
+                 raise IndexError('database and guideline not aligned')
+        
+            if guidelines["Type"][i].lower() == "binary":
+                df[category] = df[category].astype(str).str.strip().str.lower()
+                df[category] = df[category].replace(mapping)
+                if guidelines["Condition"][i].lower() == "no":
+                    df[category] = df[category].replace({0:1, 1:0})
 
-    for category, rules in config.items():
 
-        if rules["type"] == "binary":
-            col_clean = df[category].astype(str).str.strip().str.lower()
-            mapping = {k.lower(): v for k, v in rules["mapping"].items()}
-            df[category] = col_clean.replace(mapping).astype(int)
+                
+            elif guidelines["Type"][i].lower() == "numeric":
+                   df[category] = df[category].between(guidelines["Min"][i], guidelines["Max"][i]).astype(int)
+
             
+        return df[list(guidelines["Variable"])]
 
-        elif rules["type"] == "numeric":
-            healthy_range = rules.get("healthy_range") #avoids key error if healthy_range ia not defiend.
-            if isinstance(healthy_range, (list, tuple)) and len(healthy_range) >= 2:
-                df[category] = df[category].between(healthy_range[0], healthy_range[1]).astype(int)
+    except:
+        raise ImportError('add exception')
 
-            else:
-                raise ValueError(f"Category '{category}' is numeric but healthy_range is not a list: {healthy_range}")
-
-            
-    return df[list(config.keys())]
 
 
 data = pd.read_csv("enhanced_anxiety_dataset.csv")
 guidelines = pd.read_excel("health_guidelines.xlsx")
 
 
-config = {
-    "Physical Activity (hrs/week)": {
-        "type": "numeric",
-        "healthy_range": [3,5]
-        
-    },
-    "Alcohol Consumption (drinks/week)": {
-        "type": "numeric",
-        "healthy_range": [5,7]
-       
-    },
-    "Family History of Anxiety": {
-        "type": "binary",
-        "mapping": {"yes": 1, "no": 0}
-    }
-}
-
-result = apply_binary(data, config)
-print(result)
